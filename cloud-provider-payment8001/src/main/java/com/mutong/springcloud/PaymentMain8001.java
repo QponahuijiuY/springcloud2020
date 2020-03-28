90ffee941a790ad1399704d10916817e93a391c1
QponahuijiuY <1530201142@qq.com>
2020-03-28 21:36:27 +0800

    支付模块

---
+++ cloud-provider-payment8001/src/main/java/com/mutong/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.mutong.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @Author: Mutong
+ * @Date: 2020-03-27 11:31
+ * @time_complexity: O()
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
